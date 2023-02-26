2b09d80e580229546c41047e5e831cd6de87bf51
Marine <mariine.becker@gmail.com>
2023-02-26 12:03:10 +0100

    ajout page categorie console tag

---
+++ App/vue/v_categorie.php
@@ -0,0 +1,47 @@
+<section id="visite">
+    <aside id="categories">
+        <ul>
+            <?php
+            foreach ($lesCategories as $uneCategorie) {
+                $idCategorie = $uneCategorie['id'];
+                $libCategorie = $uneCategorie['nom'];
+            ?>
+                <li>
+                    <a href=index.php?uc=visite&categorie=<?php echo $idCategorie ?>&action=voirCategorie><?php echo $libCategorie ?></a>
+                </li>
+            <?php
+            }
+            ?>
+        </ul>
+
+    </aside>
+
+    <section id="jeux">
+
+        <?php
+        foreach ($lesJeux as $unJeu) {
+            $id = $unJeu['id'];
+            $nom = $unJeu['nom'];
+            $etat = $unJeu['etat_jeu'];
+            $categorie = $unJeu['categorie_id'];
+            // $description = $unJeu['description'];
+            $prix = $unJeu['prix'];
+            $image = $unJeu['image'];
+        ?>
+            <article>
+                <img src="public/images/jeux/<?= $image ?>" alt="Image de <?= $nom; ?>" />
+                <p><?= $nom ?></p>
+                <p><?= $etat ?></p>
+                <p><?= "Prix : " . $prix . " Euros" ?>
+                <br>
+                <a href="index.php?uc=jeu&action=consulter&id= <?= $id ?>" title="Voir le jeu">Voir le jeu </a>
+                <a href="index.php?uc=visite&categorie=<?= $categorie ?>&jeu=<?= $id ?>&action=ajouterAuPanier">
+                    <img src="public/images/mettrepanier.png" title="Ajouter au panier" class="add" />
+                </a>
+                </p>
+            </article>
+        <?php
+        }
+        ?>
+    </section>
+</section>
